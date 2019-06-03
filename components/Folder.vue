2c99e320a3d2438c741f4c78655b40d5b318f033
Ruurd Sinnema <ruurd22@gmail.com>
2019-06-03 14:03:07 +0200

    Added folder component

---
+++ components/Folder.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="col-sm module mid">
+    <h2>{{allData}}</h2>
+  </div>
+  <!-- <div>{{allData}}</div> -->
+</template>
+
+<script>
+export default {
+  props: {
+    allData: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background-color: #ececec;
+
+  h1 {
+    font-family: OpenSans;
+    font-weight: bold;
+    color: #424242;
+  }
+
+  .module {
+    background: url("~assets/img/recent-folder.png");
+    background-size: cover;
+    width: 150px;
+    height: 75px;
+    margin: 75px 75px 75px 75px;
+    position: relative;
+  }
+
+  .mid h2 {
+    font-family: "Roboto", sans-serif;
+    font-weight: 900;
+    color: white;
+    text-transform: uppercase;
+    margin-top: 50px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    font-size: 1rem;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
+
