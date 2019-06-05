d5711bb6db6bc8037a9750d7c39fb231425ac721
Ruurd Sinnema <ruurd22@gmail.com>
2019-06-05 09:29:44 +0200

    Deleted svg filling folder image

--- components/Folder.vue
+++ components/Folder.vue
@@ -1,20 +1,12 @@
 <template>
-  <div class="row">
-    <b-col class="recent-col" v-for="item in allData.slice(0, 4)" :key="item.id">
-      <svg class="crop-shapes">
-        <defs>
-          <pattern id="img1" patternUnits="userSpaceOnUse" width="100%" height="100%">
-            <image xlink:href="@/assets/img/screen.png" x="-30" y="-30" width="380" height="267"></image>
-          </pattern>
-        </defs>
-
-        <svg fill="url(#img1)" viewBox="0 0 278.62 187.82">
-          <g>
-            <path
-              d="M101.66,187.82H10a10,10,0,0,1-10-10V10A10,10,0,0,1,10,0H92.45a10,10,0,0,1,10,10v4H268.62a10,10,0,0,1,10,10h0V177.81a10,10,0,0,1-10,10Z"
-              transform="translate(0 0)"
-            ></path>
-          </g>
+  <div class="contents">
+    <b-col class="col" v-for="item in allData.slice(0, 4)" :key="item.id">
+      <svg class="recent-files">
+        <svg viewBox="0 0 278.62 187.82" class="recent-files">
+          <path
+            d="M101.66,187.82H10a10,10,0,0,1-10-10V10A10,10,0,0,1,10,0H92.45a10,10,0,0,1,10,10v4H268.62a10,10,0,0,1,10,10h0V177.81a10,10,0,0,1-10,10Z"
+            transform="translate(0 0)"
+          ></path>
         </svg>
       </svg>
       <h2 class="item-name">{{item.name}}</h2>
@@ -34,10 +26,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.recent-col {
+.contents {
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-wrap: wrap;
+
+  .recent-files svg {
+    fill: #424242;
+    margin-top: 50px;
+  }
 
   .item-name {
     width: 150px;
@@ -45,27 +41,14 @@ export default {
     font-weight: 900;
     color: #ffffff;
     text-transform: uppercase;
-    // margin-top: 50px;
     position: absolute;
-    // top: 50%;
-    // left: 50%;
+    top: 50%;
+    left: 50%;
     font-size: 1rem;
-    // transform: translate(-50%, -50%);
-
     display: flex;
     text-align: center;
-    justify-content: center;
+    justify-content: space-around;
   }
-}
-
-.crop-shapes {
-  width: 100%;
-  // position: absolute;
-  // right: 0;
-  // left: 0;
-  // top: 0;
-  // bottom: 0;
-  // margin: auto;
 }
 </style>
 
