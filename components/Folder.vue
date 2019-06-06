800f35dc3b8e959659eeae0d2df2baec251587c6
Ruurd Sinnema <ruurd22@gmail.com>
2019-06-06 11:00:45 +0200

    staged changes

--- components/Folder.vue
+++ components/Folder.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="row">
-    <div class="column" v-for="item in allData.slice(0, 4)" :key="item.id">
-      <h2 class="item-name">{{item.name}}</h2>
+  <div class="row folders">
+    <div class="col">
+      <div class="column" v-for="item in allData.slice(0, 4)" :key="item.id">
+        <h2 class="item-name">{{item.name}}</h2>
+      </div>
     </div>
   </div>
 </template>
@@ -19,34 +21,47 @@ export default {
 
 <style lang="scss" scoped>
 .row {
-  .column {
-    background-image: url("~assets/img/map-icon.svg");
-    background-size: cover;
-    height: 136px;
-    width: 203px;
-    margin: 10px 0 0 10px;
-    position: relative;
-
-    .item-name {
-      font-family: "Roboto", sans-serif;
-      font-weight: 900;
-      color: white;
-      margin: 0;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      font-size: 16px;
-      transform: translate(-50%, -50%);
-      display: flex;
-      flex-wrap: wrap;
-    }
+  &.folders {
+    max-width: 1100px;
+    width: 100%;
   }
-  .column:nth-child(1) {
-    margin-left: 0;
+  .col {
+    display: flex;
+    justify-content: space-between;
+
+    .column {
+      background-image: url("~assets/img/map-icon.svg");
+      background-size: cover;
+      height: 136px;
+      width: 203px;
+      margin: 10px 0 0 10px;
+      position: relative;
+
+      .item-name {
+        font-family: "Roboto", sans-serif;
+        font-weight: 900;
+        color: white;
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        font-size: 16px;
+        transform: translate(-50%, -50%);
+        display: flex;
+        flex-wrap: wrap;
+      }
+    }
+    .column:nth-child(1) {
+      margin-left: 0;
+    }
   }
 }
 .col-sm:first-child {
   margin-left: 0;
+}
+
+.folders {
+  justify-content: center;
 }
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,19 @@
 <template>
-  <b-container ref="container">
-    <b-row>
-      <b-col>
+  <div class="container" ref="mainContainer">
+    <div class="row">
+      <div class="col">
         <div class="recent-text">Recent folders</div>
-      </b-col>
-    </b-row>
-    <Folder :allData="data"/>
-  </b-container>
+      </div>
+    </div>
+    <div class="row folders">
+      <div class="col">
+        <div class="column" v-for="item in data.slice(0, 4)" :key="item.id">
+          <h2 class="item-name">{{item.name}}</h2>
+        </div>
+      </div>
+    </div>
+    <!-- <Folder :allData="data"/> -->
+  </div>
 </template>
 
 <script>
@@ -21,9 +28,11 @@ export default {
   watch: {
     getRefFromStore() {
       if (this.$store.state.stateStore.sidebarStatus) {
-        this.$refs.container.style.width = "calc(100% - 300px)";
+        console.log("true?");
+        this.$refs.mainContainer.style.width = "calc(100% - 300px)";
       } else {
-        this.$refs.container.style.width = "100%";
+        console.log("false?");
+        this.$refs.mainContainer.style.width = "100%";
       }
     }
   },
@@ -45,16 +54,57 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.header {
+.container {
+  padding: 0px;
+  margin: 0px;
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+  .row {
+    width: 100%;
+    max-width: 1100px;
+    .recent-text {
+      width: 100%;
 
-  .recent-text {
-    margin-left: 10px;
-    font-size: 16px;
-    font-weight: bold;
-    color: #424242;
-    padding-left: 0px;
+      font-size: 16px;
+      font-weight: bold;
+      color: #424242;
+    }
+  }
+
+  .folders {
+    max-width: 1100px;
+    width: 100%;
+  }
+  .col {
+    display: flex;
+    justify-content: space-between;
+
+    .column {
+      background-image: url("~assets/img/map-icon.svg");
+      background-size: cover;
+      height: 136px;
+      width: 203px;
+      margin: 10px 0 0 10px;
+      position: relative;
+
+      .item-name {
+        font-family: "Roboto", sans-serif;
+        font-weight: 900;
+        color: white;
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        font-size: 16px;
+        transform: translate(-50%, -50%);
+        display: flex;
+        flex-wrap: wrap;
+      }
+    }
+    .column:nth-child(1) {
+      margin-left: 0;
+    }
   }
 }
 </style>
